2a668feef884c9b473d7f4c4ec7b5fbbd6f0a8d6
fridolph <249121486@qq.com>
2018-04-06 12:13:49 +0800

    服务端。mock数据

---
+++ server/mock/home/ad.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        title: '培训1',
+        img: 'http://p6pzeyeut.bkt.clouddn.com/image/01.png',
+    },
+    {
+        title: '培训2',
+        img: 'http://p6pzeyeut.bkt.clouddn.com/image/02.png',
+    },
+    {
+        title: '培训3',
+        img: 'http://p6pzeyeut.bkt.clouddn.com/image/03.png',
+    }
+]
---
+++ server/mock/home/lessonList.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    img: '//10.url.cn/qqcourse_logo_ng/ajNVdqHZLLAjvIxnyqg65OO3M4QUnF1pnThUpc2HamYn4yptuyNLUk43Vzdg1rUHBA1yYD8mqRA/220',
+    lesson: 'Web前端开发之HTML+CSS精英课堂【渡一教育】',
+    price: '免费'
+  },
+  {
+    img: '//10.url.cn/qqcourse_logo_ng/ajNVdqHZLLCSRclnOD3UsiaQrpEibcAoloC6ticicwNH7FmIlOs7Wia5DBNsXlgZlltia5Hic2AM4yNa5A/220',
+    lesson: 'WEB前端开发知识模块精讲速成班html/css3/H5',
+    price: '¥2899.00'
+  },
+  {
+    img: '//10.url.cn/qqcourse_logo_ng/ajNVdqHZLLC9LKrHsWjNR0Mz6iauQtKJOia2DVT4SbXPspFEgq3habeicpAUhPUceltv9fYhxgatEw/220',
+    lesson: 'WEB前端教程（HTML/CSS/JavaScript/表单验证/JQuery/Bootstrap）',
+    price: '¥6.00'
+  },
+  {
+    img: '//10.url.cn/qqcourse_logo_ng/ajNVdqHZLLDnJIfDujDSZZT3iczXxH81ibgXD78Zic8DHs8vN7RmOQTJVDnedhnQ7xgoH4WWwz7o7M/220',
+    lesson: 'HTML+CSS入门',
+    price: '¥99.00'
+  },
+  {
+    img: '//10.url.cn/qqcourse_logo_ng/ajNVdqHZLLBQ3UH9FXCeYSjoX5tlz0yClbZXeF7CWbKyHhxdfmohfkm7QbkpWvVhqgqV5t1xVics/220',
+    lesson: '学会HTML5游戏的图集动画、时间轴动画、骨骼动画制作(JS)',
+    price: '¥12.00'
+  },
+  {
+    img: '//10.url.cn/qqcourse_logo_ng/ajNVdqHZLLBm0LnicM1QJcEop23kFuOfBclqMiavguOuLghPaBM6eFp4BBfUWW9XUEicx28JR1jGmU/220',
+    lesson: '移动端实战课程（HTML5+CSS3）',
+    price: '¥18.00'
+  }
+]
